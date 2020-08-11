abeb5c9c05c84351388ca8575f4758ad0be1d47f
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-08-10 22:42:42 -0700

    Added current time display

---
+++ script.js
@@ -0,0 +1,26 @@
+//Current Day Displays at the top
+const $currentDay = $('#currentDay');
+
+$currentDay.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+//Presented with Hour Blocks 8 am - 6 pm
+
+//Timeblocks are color coded based on time
+
+//Past Color
+
+//Present Color
+
+//Future Color
+
+//When I click into a timeblock
+
+//Then I can enter an event
+
+//When I click tthe save button for that timeblock
+
+//Then the text for that event is saved in local storage
+
+//When I refresh the page
+
+//Then the saved events persist
